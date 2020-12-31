eb3a7512857a18cf8dd060078eefa659a6951aba
john-raber <jrab224@gmail.com>
2020-12-31 16:37:56 -0500

    react native init and setting up react-native-navigation and ios and android builds working

---
+++ android/app/src/main/java/com/rookscoring/MainActivity.java
@@ -0,0 +1,8 @@
+package com.rookscoring;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+  
+}
---
+++ components/Options.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const Options = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hi John!</Text>
+    </SafeAreaView>
+  );
+};
